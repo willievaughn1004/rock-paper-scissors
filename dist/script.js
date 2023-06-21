2cc50be0ebef17bfb6a81cfe49ec39726ae515fd
Willie Vaughn <willievaughn1004@gmail.com>
2023-06-21 18:13:57 -0400

    Finish project
    I added the ability for the UI to update based on the player's choices
    and the computer's choices.
    
    I also made it so the game ends after either the player or computer wins
    and then it resets.

--- dist/script.js
+++ dist/script.js
@@ -1,86 +1,100 @@
 // Variables
 
 const gameChoices = document.querySelectorAll(".game-choices");
-const rock = document.querySelector("#rock");
-const paper = document.querySelector("#paper");
-const scissors = document.querySelector("#scissors");
+const playerSymbol = document.querySelector(".player-symbol");
+const computerSymbol = document.querySelector(".computer-symbol");
+const roundStatus = document.querySelector("#round-status");
+const roundMessage = document.querySelector("#round-message");
+const playerScore = document.querySelector("#player-score");
+const computerScore = document.querySelector("#computer-score");
 
 // Functions
 
 const getPlayerChoice = (event) => {
   const button = event.target.closest("button");
   if (button) {
-    playRound(button.getAttribute("id"), getComputerChoice())
+    return [button.getAttribute("id"), button.innerHTML];
   }
-
 };
 
 const getComputerChoice = () => {
-  let gameArr = ["rock", "paper", "scissors"];
+  let gameArr = [
+    ["Rock", '<i class="fa-regular fa-hand-back-fist"></i>'],
+    ["Paper", '<i class="fa-regular fa-hand"></i>'],
+    ["Scissors", '<i class="fa-regular fa-hand-scissors"></i>'],
+  ];
   return gameArr[Math.floor(Math.random() * gameArr.length)];
 };
 
-const playRound = (playerSelection, computerSelection) => {
-  
-  if (playerSelection === computerSelection) {
-    return "Draw! Go again!";
+const playRound = (event) => {
+  const playerSelection = getPlayerChoice(event);
+  const computerSelection = getComputerChoice();
+
+  playerSymbol.innerHTML = playerSelection[1];
+  computerSymbol.innerHTML = computerSelection[1];
+
+  if (playerSelection[0] === computerSelection[0]) {
+    roundStatus.innerText = "It's a tie!";
+    roundMessage.innerText = `${playerSelection[0]} ties with ${playerSelection[0]}`;
+    return;
   }
 
-  if (playerSelection === "rock") {
-    if (computerSelection === "paper") {
-      return "You Lose! Paper beats Rock";
+  if (playerSelection[0] === "Rock") {
+    if (computerSelection[0] === "Paper") {
+      roundStatus.innerText = "You Lose!";
+      roundMessage.innerText = "Paper beats Rock";
+      computerScore.innerText++;
     } else {
-      return "You Win! Rock beats Scissors";
+      roundStatus.innerText = "You Win!";
+      roundMessage.innerText = "Rock beats Scissors";
+      playerScore.innerText++;
     }
   }
 
-  if (playerSelection === "paper") {
-    if (computerSelection === "scissors") {
-      return "You Lose! Scissors beats Paper";
+  if (playerSelection[0] === "Paper") {
+    if (computerSelection[0] === "Scissors") {
+      roundStatus.innerText = "You Lose!";
+      roundMessage.innerText = "Scissors beats Paper";
+      computerScore.innerText++;
     } else {
-      return "You Win! Paper beats Rock";
+      roundStatus.innerText = "You Win!";
+      roundMessage.innerText = "Paper beats Rock";
+      playerScore.innerText++;
     }
   }
 
-  if (playerSelection === "scissors") {
-    if (computerSelection === "rock") {
-      return "You Lose! Rock beats Scissors";
+  if (playerSelection[0] === "Scissors") {
+    if (computerSelection[0] === "Rock") {
+      roundStatus.innerText = "You Lose!";
+      roundMessage.innerText = "Rock beats Scissors";
+      computerScore.innerText++;
     } else {
-      return "You Win! Scissors beats Paper";
-    }
-  }
-};
-
-const game = (rounds) => {
-  let playerWins = 0;
-  let computerWins = 0;
-
-  for (let i = rounds; i > 0; i--) {
-    let round = playRound(getPlayerChoice(), getComputerChoice());
-
-    if (/win/gi.test(round)) {
-      alert(round);
-      playerWins++;
-    } else if (/lose/gi.test(round)) {
-      alert(round);
-      computerWins++;
-    } else {
-      alert(round);
-      i++;
+      roundStatus.innerText = "You Win!";
+      roundMessage.innerText = "Scissors beats Paper";
+      playerScore.innerText++;
     }
   }
 
-  if (playerWins > computerWins) {
-    alert("You won best of five rounds!");
-  } else if (playerWins === computerWins) {
-    alert("You guys tied!");
-  } else {
-    alert("You lost best of five rounds!");
+  if (parseInt(computerScore.innerText) === 5 || parseInt(playerScore.innerText) === 5) {
+    if (playerScore.innerText > computerScore.innerText) {
+      alert("You won!");
+    } else {
+      alert("You lost...");
+    }
+
+    playerSymbol.innerHTML = '<i class="fa-solid fa-question"></i>';
+    computerSymbol.innerHTML = '<i class="fa-solid fa-question"></i>';
+    roundStatus.innerText = "Choose your weapon";
+    roundMessage.innerText = "First to score 5 points wins the game";
+    playerScore.innerText = 0;
+    computerScore.innerText = 0;
+
   }
+
+  console.log(computerScore.innerText + playerScore.innerText)
+
 };
 
 // Event Listeners
 
-gameChoices.forEach((choice) =>
-  choice.addEventListener("click", getPlayerChoice)
-);
+gameChoices.forEach((choice) => choice.addEventListener("click", playRound));
